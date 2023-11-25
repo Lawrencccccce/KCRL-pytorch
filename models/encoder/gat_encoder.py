7b669cd97fe947b681a1ef52b62eff98f774c1b8
Lawrencccccce <xiaoxuan.li.w@gmail.com>
2023-11-25 14:48:00 +1100

    comment out all the print statements

--- models/encoder/gat_encoder.py
+++ models/encoder/gat_encoder.py
@@ -11,17 +11,17 @@ def attn_head(seq, out_sz, activation, in_drop=0.0, coef_drop=0.0, residual=Fals
         seq = F.dropout(seq, in_drop)                               # shape (batch_size, max_length, input_dimension)
 
     seq = seq.permute(0, 2, 1)                                      # shape (batch_size, input_dimension, max_length)
-    print(f'seq shape: {seq.shape}')
+    # print(f'seq shape: {seq.shape}')
     seq_fts = nn.Conv1d(seq.size(1), out_sz, 1, bias=False)(seq)    # shape (batch_size, out_sz, max_length)
 
-    print(f'seq_fts shape: {seq_fts.shape}')
+    # print(f'seq_fts shape: {seq_fts.shape}')
 
     f_1 = nn.Conv1d(out_sz, 1, 1)(seq_fts)                          # shape (batch_size, 1, max_length)   
     f_2 = nn.Conv1d(out_sz, 1, 1)(seq_fts)                          # shape (batch_size, 1, max_length)
-    print(f'f_1 shape: {f_1.shape}')
-    print(f'f_2 shape: {f_2.shape}')
+    # print(f'f_1 shape: {f_1.shape}')
+    # print(f'f_2 shape: {f_2.shape}')
     logits = f_1.permute(0, 2, 1) + f_2                             # shape (batch_size, max_length, max_length)
-    print(f'logits shape: {logits.shape}')
+    # print(f'logits shape: {logits.shape}')
     coefs = F.softmax(F.leaky_relu(logits), dim=-1)                 
 
     seq_fts = seq_fts.permute(0, 2, 1)                              # shape (batch_size, max_length, out_sz)
@@ -30,8 +30,8 @@ def attn_head(seq, out_sz, activation, in_drop=0.0, coef_drop=0.0, residual=Fals
     if in_drop != 0.0:
         seq_fts = F.dropout(seq_fts, in_drop)                       # shape (batch_size, max_length, out_sz)
 
-    print(f'coefs shape: {coefs.shape}')
-    print(f'seq_fts shape: {seq_fts.shape}')
+    # print(f'coefs shape: {coefs.shape}')
+    # print(f'seq_fts shape: {seq_fts.shape}')
 
     vals = torch.bmm(coefs, seq_fts)
     ret = vals + seq_fts
@@ -63,7 +63,7 @@ class GATEncoder(nn.Module):
         output shape: (batch_size, max_length, input_embed) 
         """
         head_hidden_dim = int(self.hidden_dim / self.num_heads)
-        print(f'head_hidden_dim: {head_hidden_dim}')
+        # print(f'head_hidden_dim: {head_hidden_dim}')
         x = inputs
         for _ in range(self.num_stacks):
             heads = []
